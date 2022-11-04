1f0481390fab4ed2295c7f1d4a18ab1bd4d757e7
RizalSwikey <rizalakbarsyah@gmail.com>
2022-11-04 15:46:03 +0700

    finishing chapter_6

---
+++ test.py
@@ -0,0 +1,21 @@
+# for i in range (1000):
+#     print ('We Like python turtle!')
+
+# for i in ['January','February','Maret','April','Mei','June','July','August', 'September','October','November','Desember']:
+#     month = ('One of the months of the year is ' + i) 
+#     print (month)
+
+# import turtle
+# turtle.Screen()
+# swikey = turtle.Turtle()
+# swikey.left(3645)
+# turtle.mainloop()
+
+xs = [12,10,32,3,66,17,42,99,20]
+for i in xs :
+    print (i, '=', i**2)
+total = 0
+for i in xs:
+    total = total + i
+print (total)
+    
